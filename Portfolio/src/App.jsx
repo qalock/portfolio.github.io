f1155e9e9963decab6788932d3aec010a7c0c653
qalock <surajpandey2007@gmail.com>
2026-03-05 20:32:33 +0530

    favicon added

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
@@ -10,28 +10,46 @@ import Footer from "./components/Footer";
 import AOS from "aos";
 import { useEffect } from "react";
 
-
-export default function App(){
-
+export default function App() {
   useEffect(() => {
     AOS.init({
       duration: 1000,
       once: true,
     });
   }, []);
-    return(
-        <React.Fragment>
-            <BrowserRouter basename="/portfolio.github.io">
-            <Navbar/>
-            <ToastContainer position="top-right" autoClose={3000} hideProgressBar={false} newestOnTop closeOnClick pauseOnHover theme="dark"/>
-            <Routes>
-                <Route path="/" element={ <Home/> } />
-                <Route path="/projects" element={ <Projects/> } />
-                <Route path="/about" element={ <About/> } />
-                <Route path="/contact" element={ <Contact/> } />
-            </Routes>
-            <Footer/>
-            </BrowserRouter>
-        </React.Fragment>
-    )
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1500);
+  }, []);
+
+  if (loading) {
+    return <Loader />;
+  }
+  return (
+    <React.Fragment>
+      <BrowserRouter basename="/portfolio.github.io">
+        <Navbar />
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop
+          closeOnClick
+          pauseOnHover
+          theme="dark"
+        />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </React.Fragment>
+  );
 }
